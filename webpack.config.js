4152fe6ca3674807c8a4b3303b0f665967818fb1
大板栗 <576839360@qq.com>
2019-07-03 11:49:41 +0800

    🎉 Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HTML = require('html-webpack-plugin');
+
+const configure = {
+    entry: {
+        app: path.resolve('src/app'),
+    },
+    output: {
+        path: path.resolve('dist'),
+        filename: `js/[name].js?[hash:8]`,
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        alias: {
+            '@src': path.resolve(__dirname, `src`),
+            '@app': path.resolve(__dirname, `src/app`),
+            '@common': path.resolve(__dirname, `src/common`),
+            '@modules': path.resolve(__dirname, `src/modules`),
+        },
+    },
+    module: {
+        rules: [{
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: require.resolve('ts-loader'),
+                options: {
+                    transpileOnly: true,
+                },
+            }],
+        }, {
+            test: /\.(mp3|mp4)$/,
+            use: [{
+                loader: `url-loader`,
+                options: {
+                    limit: 5120,
+                    name: `medias/[name].[ext]?[hash:8]`,
+                },
+            }],
+        }, {
+            test: /\.(svg|png|gif|jpe?g)$/,
+            use: [{
+                loader: `url-loader`,
+                options: {
+                    limit: 5000,
+                    name: `images/[name].[ext]?[hash:8]`,
+                },
+            }],
+        }, {
+            test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/',
+                    emitFile: false
+                }
+            }]
+        }],
+    },
+    plugins: [
+        new HTML({
+            filename: `index.html`,
+            template: path.resolve('src/template/index.html'),
+            inject: true,
+        }),
+    ],
+    devtool: 'cheap-module-source-map',
+};
+
+module.exports = configure;
